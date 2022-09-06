391dd0b36dd300584d149e0edb3012ad5d54d5b5
shihabcse1 <tomdshihab@gmail.com>
2022-09-06 12:00:50 +0600

    conneceted with database completed

--- index.js
+++ index.js
@@ -20,6 +20,33 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log("Database Connected");
+        //   const database = client.db("sample_mflix");
+        //   const movies = database.collection("movies");
+        //   // query for movies that have a runtime less than 15 minutes
+        //   const query = { runtime: { $lt: 15 } };
+        //   const options = {
+        //     // sort returned documents in ascending order by title (A->Z)
+        //     sort: { title: 1 },
+        //     // Include only the `title` and `imdb` fields in each returned document
+        //     projection: { _id: 0, title: 1, imdb: 1 },
+        //   };
+        //   const cursor = movies.find(query, options);
+        //   // print a message if no documents were found
+        //   if ((await cursor.count()) === 0) {
+        //     console.log("No documents found!");
+        //   }
+        //   // replace console.dir with your callback to access individual elements
+        //   await cursor.forEach(console.dir);
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
     res.send("Hello Doctor!");
 });
