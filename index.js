7c668f9521f57b558fb63da0a29e1c29db8b6a8d
shihabcse1 <tomdshihab@gmail.com>
2022-09-06 11:55:08 +0600

    conneceted with server completed

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express"); // importing express
 const cors = require("cors"); // importing middleware
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 require("dotenv").config(); // importing dotenv
 const port = process.env.PORT || 5000;
 
 // we are telling middleware to use those
 app.use(cors());
 app.use(express.json()); // express.json() help to parse the data which we get through body
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pitr9if.mongodb.net/?retryWrites=true&w=majority`;
+
+//console.log(uri);
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
     res.send("Hello Doctor!");
