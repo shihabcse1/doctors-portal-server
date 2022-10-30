cd3f62ebf3eb5838a9d049d26ebec91d0f21a292
shihabcse1 <tomdshihab@gmail.com>
2022-10-30 12:03:15 +0600

    root response text change

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Hello Doctor!");
+    res.send("Server is Running!");
 });
 
 app.listen(port, () => {
